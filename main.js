d08b1eb3aefa46e701db73bbfc6f74695e5b9c89
Josh <joshua.e.green2023@gmail.com>
2026-05-05 23:21:44 -0400

    refactor: update card retrieval and logging in main.js

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
-// SDK import
-const TCGdex = require ('tcgdex/sdk').default;
+const TCGdex = require('@tcgdex/sdk').default;
 const tcgdex = new TCGdex('en');
 
 
-// Getting data for specific card
-
-const card = await tcgdex.card.get('swsh3-136');
-
-// getting image for card
-
-const cardpng = card.getImageURL ('high', 'png');
-
 (async () => {
-const card = await tcgdex.card.get('swsh3-136');
-console.log(card.name); // "Furret"
+    const card = await tcgdex.card.get('swsh3-133');
+    console.log(card.set);
+    
 })();
 
-console.log (card);
-console.log (cardpng);
-console.log ('30');
