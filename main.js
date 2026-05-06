e68803aa1774845c13b4a0e1d018de39f29f6cce
Josh <joshua.e.green2023@gmail.com>
2026-05-05 23:51:27 -0400

    fix : updated script to pull properly from tcgdex sdk

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const tcgdex = new TCGdex('en');
 
 
 (async () => {
-    const card = await tcgdex.card.get('swsh3-133');
-    console.log(card.set);
-    
+    const card = await tcgdex.card.get('swsh3-122');
+    console.log(card.name);
+    console.log(card.getImageURL('high', 'png'));
+
 })();
 
